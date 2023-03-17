0c0110f289affab6ddc108b01595f3edf94d17be
maikolcw <whysoraccoony@gmail.com>
2023-03-17 15:43:20 -0700

    added and tested generate_iso method

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import glob
 import hashlib
 import os
 import shutil
+
+# imports for pycdlib for iso management
+from pycdlib.pycdlibexception import PyCdlibException
+
+try:
+    from cStringIO import StringIO as BytesIO
+except ImportError:
+    from io import BytesIO
+import pycdlib
 
 TEMP_DIR = 'tmp\\optical-test'
 ISO_NAME = 'optical-test.iso'
@@ -13,6 +20,10 @@ SAMPLE_FILE_PATH = 'usr\\share\\example-content'
 SAMPLE_FILE = 'Ubuntu_Free_Culture_Showcase.txt'
 MD5SUM_FILE = 'optical_test.md5'
 START_DIR = os.getcwd()
+
+# Some helper paths
+FULL_PATH_TO_TEMP_DIR_MD5SUM_FILE = os.path.join(START_DIR, TEMP_DIR, MD5SUM_FILE)
+FULL_PATH_TO_SAMPLE_FILE_PATH_SAMPLE_FILE = os.path.join(START_DIR, SAMPLE_FILE_PATH, SAMPLE_FILE)
 
 
 def create_working_dirs():
@@ -32,10 +43,9 @@ def create_working_dirs():
 def get_sample_data():
     # Get our sample files
     print("Getting sample files from % s ..." % SAMPLE_FILE_PATH)
-    src = os.path.join(START_DIR, SAMPLE_FILE_PATH, SAMPLE_FILE)
     dst = os.path.join(START_DIR, TEMP_DIR)
     try:
-        shutil.copy2(src, dst)
+        shutil.copy2(FULL_PATH_TO_SAMPLE_FILE_PATH_SAMPLE_FILE, dst)
         return 1
     except IOError as error:
         print(error)
@@ -57,10 +67,10 @@ def generate_md5():
             with open(file_name, 'rb') as file_to_md5:
                 for chunk in iter(lambda: file_to_md5.read(4096), b""):
                     md5_hasher.update(chunk)
-            with open(os.path.join(cur_dir, MD5SUM_FILE), 'a', newline='\n') as md5_file:
+            with open(FULL_PATH_TO_TEMP_DIR_MD5SUM_FILE, 'a', newline='\n') as md5_file:
                 md5_file.write("% s  % s\n" % (md5_hasher.hexdigest(), file_name))
         # Check the sums for paranoia’s sake
-        rt = check_md5(os.path.join(cur_dir, MD5SUM_FILE))
+        rt = check_md5(FULL_PATH_TO_TEMP_DIR_MD5SUM_FILE)
     except OSError as error:
         print(error)
         return 0
@@ -94,10 +104,48 @@ def check_md5(md5_file):
     return 1
 
 
-# Press the green button in the gutter to run the script.
+def generate_iso():
+    # Generate ISO image
+    print("Creating ISO Image ...")
+    try:
+        # create pycdlib object and create new iso with parameters
+        iso = pycdlib.PyCdlib()
+        # interchange dictates rules on names of files with level 4 being most relaxed
+        # rock ridge extension, selection of '1.09', '1.10', or '1.12' with 1.09 most compatible
+        # joliet Microsoft extension, selection of 1, 2, or 3. 3 is most common
+        iso.new(interchange_level=4, rock_ridge='1.09', joliet=3)
+
+        target_file_name = SAMPLE_FILE.split(".")[0]
+
+        prepare_file_handle = open(FULL_PATH_TO_SAMPLE_FILE_PATH_SAMPLE_FILE, 'rb')
+        prepare_file_body = prepare_file_handle.read()
+
+        # add file to iso. First argument takes file like object
+        # Second argument, length of content to add to ISO (required)
+        # Third argument, location of the file on the resulting ISO(iso_path), must be absolute path,
+        # all intermediate directories must exist. Some other restrictions on length and format like
+        # ending with semicolon and version number, please look at pycdlib docs for more detail
+        # rr_name required if using rock ridge extension
+        iso.add_fp(BytesIO(prepare_file_body), len(prepare_file_body), '/' + target_file_name.upper() + ';1',
+                   rr_name=target_file_name)
+
+        # write out the iso / mastering
+        iso.write(ISO_NAME)
+        # close out pycdlib object, freeing resources. Object can be reused for new or existing ISO
+        iso.close()
+
+        prepare_file_handle.close()
+        return 1
+    except PyCdlibException as error:
+        print(error)
+        return 0
+
+
 if __name__ == '__main__':
     create_working_dirs() or print("Failed to create working directories")
     get_sample_data() or print("Failed to copy sample data")
     generate_md5() or print("Failed to generate initial md5")
+    generate_iso()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
