87cb29f14362d6b968ce7510ce13f9ee17a1bf43
maikolcw <whysoraccoony@gmail.com>
2023-03-18 16:28:14 -0700

    final regression testing and added some documentation

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ comments, but I had some challenges.
 I had some troubles finding the equivalent commands from the Python libraries. If I can't find a Python library for a 
 task, I opted for command line options. But since I developed this program in Windows, I used Windows commands in 
 os.system and subprocess.call for powershell, example burning an image and mounting. Some areas of the code might not be
-1 for 1, but is the closest I can get. 
+1 for 1, but it is the closest I can get. 
 I tested quite a bit for the program to execute gracefully, but there might be some errors in areas that require a 
 physical ROM drive because I didn't have access to one. I had to change the logic and order in those and some other 
 areas, but it should behave very similarly. The eject command is untested. 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# This is a sample Python script.
+# This Python program tries to replicate optical_write_test.sh as best as possible in a Windows environment without
+# a physical drive to test
 import argparse
 import glob
 import hashlib
@@ -8,7 +9,7 @@ import subprocess
 import time
 import ctypes
 
-# Used wmi for disk ejection command
+# Used wmi for finding drives in computer and getting letter of ROM drive
 import wmi
 
 # imports for pycdlib for iso management
@@ -65,7 +66,7 @@ def generate_md5():
     try:
         path = os.path.join(START_DIR, SAMPLE_FILE_PATH)
         os.chdir(path)
-        # Assuming md5sum -- * is get all files' md5sum and put into md5 file
+        # Attempt to imitate, assuming md5sum -- * is get all files' md5sum and put into md5 file
         # Grab all file names in current directory and put in list file_names
         file_names = glob.glob('*')
         # For each file, get the md5 hash and write hash and file name to file MD5SUM_FILE
@@ -91,6 +92,9 @@ def generate_md5():
 
 
 def check_md5(md5_file):
+    # This function might fail if the md5 file has multiple lines of hash and files and some don't exist
+    # It will try to open a file that doesn't exist and return 0
+    # Caused by generate_md5 function ran more than once back to back since it adds to md5 file and not overwrite
     print("Checking md5sums ...")
     try:
         with open(md5_file, 'r') as file_to_check_md5:
@@ -158,11 +162,13 @@ def burn_iso():
     print("Sleeping 10 seconds in case drive is not yet ready ...")
     # time.sleep(10)
     print("Beginning image burn ...")
+    # This command should burn to CD/DVD/BD
     os.system("isoburn.exe /Q % s % s" % (OPTICAL_DRIVE, FULL_PATH_TO_TEMP_DIR_MD5SUM_FILE))
     # check if burning succeeded by checking if SAMPLE_FILE is present in optical drive
     # assuming burning will only burn ISO with one file called SAMPLE_FILE
-    # if not os.path.exists(OPTICAL_DRIVE + SAMPLE_FILE):
-    #     return 0
+    # Comment out this if check to skip this check when failing burn_iso
+    if not os.path.exists(OPTICAL_DRIVE + SAMPLE_FILE):
+        return 0
     if not OPTICAL_TYPE == 'cd' or OPTICAL_TYPE == 'dvd' or OPTICAL_TYPE == 'bd':
         print("Invalid type specified % s" % OPTICAL_TYPE)
         exit(1)
@@ -173,22 +179,20 @@ def check_disk():
     time_out = 300
     sleep_count = 0
     interval = 3
+    global OPTICAL_DRIVE
     # Give the tester up to 5 minutes to reload the newly created CD/DVD
     print("Waiting up to 5 minutes for drive to be mounted ...")
     while True:
         time.sleep(interval)
         sleep_count = sleep_count + interval
-        c = wmi.WMI()
         # Goes through each drive in computer, check if drive is a CD-ROM/DVD-ROM by number and if SAMPLE_FILE exist
-        for drive in c.Win32_LogicalDisk():
-            if drive.DriveType == 5 and os.path.exists(drive.DeviceID + SAMPLE_FILE):
-                global OPTICAL_DRIVE
-                OPTICAL_DRIVE = drive.DeviceID
-                break
+        # Assign new drive letter to OPTICAL_DRIVE
+        find_optical_drive()
         if os.path.exists(OPTICAL_DRIVE + SAMPLE_FILE):
             print("Drive appears to be mounted now")
             break
         else:
+            # Use powershell for mounting an ISO with given full path, can subsequently unmount at same path
             # Assuming user's powershell.exe is in system32
             subprocess.call("C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
                             " Mount-DiskImage -ImagePath % s" % FULL_PATH_TO_TEMP_DIR_ISO_NAME, shell=True)
@@ -199,27 +203,26 @@ def check_disk():
             print("WARNING: TIMEOUT Exceeded and no mount detected!")
             break
     print("Deleting original data files ...")
+    # Check to remove file
     if os.path.exists(SAMPLE_FILE):
         os.remove(SAMPLE_FILE)
     if os.path.exists(OPTICAL_DRIVE):
         os.system("dir")
         print("Disk is mounted to % s" % OPTICAL_DRIVE)
     else:
+        # Since windows mounting command is different, this just retries to mount again with same powershell command
         print("Attempting best effort to mount % s on my own" % OPTICAL_DRIVE)
         mount_pt = os.path.join(START_DIR, TEMP_DIR, "mnt")
         print("Creating temp mount: % s" % mount_pt)
         os.makedirs(mount_pt, exist_ok=True)
         print("Mounting disk to mount point ...")
-        # Can't find Python library for mount or Windows cmd equivalent so opted for this
+        # Mounting via powershell might put the mount on a drive different from OPTICAL_DRIVE
         mount_test_again = subprocess.call("C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
                                            " Mount-DiskImage -ImagePath % s" % FULL_PATH_TO_TEMP_DIR_ISO_NAME,
                                            shell=True)
-        # Goes through each drive in computer, check if drive is a CD-ROM/DVD-ROM by number and if SAMPLE_FILE exist
-        c = wmi.WMI()
-        for drive in c.Win32_LogicalDisk():
-            if drive.DriveType == 5 and os.path.exists(drive.DeviceID + SAMPLE_FILE):
-                OPTICAL_DRIVE = drive.DeviceID
-                break
+        # This mounted working ROM drive and assign letter to OPTICAL_DRIVE
+        find_optical_drive()
+        print(OPTICAL_DRIVE)
         if mount_test_again == 1:
             print("ERROR: Unable to re-mount % s!" % OPTICAL_DRIVE)
             return 0
@@ -239,12 +242,15 @@ def cleanup():
         print("Now residing in % s" % START_DIR)
         print("Cleaning up ...")
         # Assuming ISO will only be mounted in TEMP_DIR and never in mnt
+        # Unmount ISO
         if os.path.exists(FULL_PATH_TO_TEMP_DIR_ISO_NAME):
             subprocess.call("C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
                             " Dismount-DiskImage -ImagePath % s" % FULL_PATH_TO_TEMP_DIR_ISO_NAME, shell=True)
+        # Check to remove directory
         if os.path.exists(FULL_PATH_TO_TEMP_DIR):
             os.system("rmdir /s /q % s" % FULL_PATH_TO_TEMP_DIR)
         print("Ejecting spent media ...")
+        # Untested ctypes cmd to eject in Windows, set OPTICAL_DRIVE as drive to eject
         if os.path.exists(OPTICAL_DRIVE):
             ctypes.windll.WINMM.mciSendStringW(u"open % s type cdaudio alias d_drive" % OPTICAL_DRIVE, None, 0, None)
             ctypes.windll.WINMM.mciSendStringW(u"set d_drive door open", None, 0, None)
@@ -261,6 +267,20 @@ def failed(failed_message):
     exit(1)
 
 
+def find_optical_drive():
+    """
+    Checks computer for all drives, finds ROM drive by number, and if it contains SAMPLE_FILE, assign OPTICAL_DRIVE
+    with ROM drive's letter.
+    :return:
+    """
+    wmi_object = wmi.WMI()
+    global OPTICAL_DRIVE
+    for drive in wmi_object.Win32_LogicalDisk():
+        if drive.DriveType == 5 and os.path.exists(drive.DeviceID + SAMPLE_FILE):
+            OPTICAL_DRIVE = drive.DeviceID
+            return
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Assuming Windows will have default disk drive of D and D is not used by any other drive
@@ -270,10 +290,10 @@ if __name__ == '__main__':
     OPTICAL_DRIVE = args.drive
     OPTICAL_TYPE = args.type
 
-    # create_working_dirs() or failed("Failed to create working directories")
-    # get_sample_data() or failed("Failed to copy sample data")
-    # generate_md5() or failed("Failed to generate initial md5")
-    # generate_iso() or failed("Failed to create ISO image")
-    # burn_iso() or failed("Failed to burn ISO image")
-    # check_disk() or failed("Failed to verify files on optical disk")
-    # cleanup() or failed("Failed to clean up")
+    create_working_dirs() or failed("Failed to create working directories")
+    get_sample_data() or failed("Failed to copy sample data")
+    generate_md5() or failed("Failed to generate initial md5")
+    generate_iso() or failed("Failed to create ISO image")
+    burn_iso() or failed("Failed to burn ISO image")
+    check_disk() or failed("Failed to verify files on optical disk")
+    cleanup() or failed("Failed to clean up")
